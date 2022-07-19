ce426136fbb240ce5ebe270a563f18bcd229a158
Andrey <586380+andreyantipov@users.noreply.github.com>
2022-07-19 10:28:38 +0400

    add highlighter multi words suppport

--- src/app/ui/hooks/useHighlighter.tsx
+++ src/app/ui/hooks/useHighlighter.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 
-export function useHighlighter(word: string, Highlighter: React.ElementType) {
+export function useHighlighter(words: string, Highlighter: React.ElementType) {
   const highlight = (text: string) => {
-    const regex = new RegExp(`(${word})`, "gi");
-    const syllables = text.split(regex);
-
-    return syllables.map((syllable, index) => {
-      if (syllable.toLocaleLowerCase() === word.toLocaleLowerCase()) {
-        return <Highlighter key={index}>{syllable}</Highlighter>;
-      }
-
-      return <span key={index}>{syllable}</span>;
-    });
+    return words.split(' ').map(word => {
+      const regex = new RegExp(`(${word})`, "gi");
+      const syllables = text.split(regex);
+  
+      return syllables.map((syllable, index) => {
+        if (syllable.toLocaleLowerCase() === word.toLocaleLowerCase()) {
+          return <Highlighter key={index}>{syllable}</Highlighter>;
+        }
+  
+        return <span key={index}>{syllable}</span>;
+      });
+    })
   };
 
   return (text: string) => {
