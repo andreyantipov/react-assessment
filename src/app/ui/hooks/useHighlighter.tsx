fb6f130a9ae1023fd3f4aef4ad8f583fa2e26dd7
Andrey <586380+andreyantipov@users.noreply.github.com>
2022-07-19 10:56:13 +0400

    fix multi words highlighter

--- src/app/ui/hooks/useHighlighter.tsx
+++ src/app/ui/hooks/useHighlighter.tsx
@@ -2,18 +2,19 @@ import React from "react";
 
 export function useHighlighter(words: string, Highlighter: React.ElementType) {
   const highlight = (text: string) => {
-    return words.split(' ').map(word => {
-      const regex = new RegExp(`(${word})`, "gi");
-      const syllables = text.split(regex);
-  
-      return syllables.map((syllable, index) => {
-        if (syllable.toLocaleLowerCase() === word.toLocaleLowerCase()) {
-          return <Highlighter key={index}>{syllable}</Highlighter>;
-        }
-  
-        return <span key={index}>{syllable}</span>;
-      });
-    })
+    const search = words.toLocaleLowerCase().split(' ');
+    const expression = words.split(' ').map(w => `(${w})`).join('|');
+    const regex = new RegExp(expression, "gi");
+    const syllables = text.toLowerCase().split(regex);
+
+    console.log(search);
+    return syllables.map((syllable, index) => {
+      if (search.includes(syllable)) {
+        return <Highlighter key={index}>{syllable}</Highlighter>;
+      }
+
+      return <span key={index}>{syllable}</span>;
+    });
   };
 
   return (text: string) => {
